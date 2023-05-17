5ab2371aad3e2eb2d54ac5c01b5b488772184486
dmoreir1 <dmoreir1@binghamton.edu>
2023-05-17 18:48:52 -0400

    updating exercise 2

--- src/Student3/Ex2/Main.js
+++ src/Student3/Ex2/Main.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import {Header} from './Header'
+// import {Menu} from './Menu'
+// import {Footer} from './Footer'
 import './Main.css'
 
 export const Main = () => {
   return (
     <div>
-        Hello I'm student id: 1.
+      <Header></Header>
+      {/* <Menu></Menu> */}
+      {/* <Footer></Footer> */}
     </div>
+
+  //   <div className="Main">
+  //   <Header />
+  //   <Menu />
+  //   <Footer />
+  // </div>
   )
 }
