60a2a77db03f3d122d6464dba03b0e23f241cee4
dmoreir1 <dmoreir1@binghamton.edu>
2023-05-17 14:28:50 -0400

    Exercise 1 Done!

---
+++ src/Student3/Ex1/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <><div class="logo">
+          Dollar Food Store
+      </div><nav class="nav">
+              Home | Login | Support | FAQ
+          </nav></>
+  )
+}
