1f49445b852130d35c82e5ec7253cb5fa0631584
dmoreir1 <dmoreir1@binghamton.edu>
2023-05-19 17:26:27 -0400

    Exercise 5-6

--- src/Student3/Ex6/Main.js
+++ src/Student3/Ex6/Main.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import { useState} from 'react'
+import { useRef } from 'react'
 import './Main.css'
 
+
 export const Main = () => {
+  const [sentence, setSentence] = useState('');
+  const wordInputRef = useRef();
+
+  const addWord = () => {
+    const newWord = wordInputRef.current.value;
+    setSentence(prevSentence => prevSentence + ' ' + newWord);
+    wordInputRef.current.value = '';
+  };
+
+  const resetSentence = () => {
+    setSentence('');
+    wordInputRef.current.value = '';
+  };
+
   return (
     <div>
-        Hello I'm student id: 1.
+      <h1>Sentence Builder</h1>
+      <div>
+        <label htmlFor="wordInput">Add Word:</label>
+        <input type="text" id="wordInput" ref={wordInputRef} />
+        <button onClick={addWord}>Add Word</button>
+        <button onClick={resetSentence}>Reset</button>
+      </div>
+      <p>{sentence}</p>
     </div>
-  )
-}
+  );
+};
+
+export default Main;
+
+
---
+++ src/Student3/Ex6/SentenceBuilder.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Main from './Main';
+
+const SB = () => {
+  return (
+    <div>
+      < Main />
+    </div>
+  );
+};
+
+export default SB;
+
+
+
+
+
+
